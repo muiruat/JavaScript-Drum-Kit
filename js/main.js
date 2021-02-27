a44ccf87e85e1a141857378ab666ea7fe589416e
sjseo <muiruat@naver.com>
2021-02-27 17:35:43 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+function removeTransition(e) {
+    if(e.propertyName !== "transform") {
+        return;
+    }
+
+    e.target.classList.remove("playing");
+}
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -11,4 +19,6 @@ function playSound(e) {
     audio.play();
 }
 
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
